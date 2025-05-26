72d2acb5544ac83ddc5bd38bbea4ab3ee844c8b8
이명필 <zkwmdi30@gmail.com>
2025-05-26 22:30:43 +0900

    마케팅 화면추가

---
+++ pages/marketing.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="min-h-screen flex flex-col items-center justify-center bg-[#C62036] px-4"
+  >
+    <div class="text-center">
+      <div
+        class="font-extrabold text-white text-7xl md:text-8xl leading-tight mb-2"
+      >
+        단어는 매일
+      </div>
+      <div
+        class="font-extrabold text-white text-6xl md:text-7xl leading-tight mb-8"
+      >
+        봐야 눕니다
+      </div>
+      <div class="font-bold text-white text-3xl md:text-4xl mb-4">
+        외우려 하지 마세요
+      </div>
+      <div class="font-bold text-white text-2xl md:text-3xl">
+        익숙해질 때까지 그냥 매일 보세요
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "default",
+});
+</script>
